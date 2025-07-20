63517ea30286f4b30f7162a9d39212e13b99dd81
mohammedd2002 <168116874+mohammedd2002@users.noreply.github.com>
2025-07-20 13:03:01 +0300

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+
+    public function admin(): Factory
+{
+    return $this->state(function (array $attributes) {
+        return [
+            'is_admin' => 1,
+        ];
+    });
+    
+}
+
+ public function configure(): static
+    {
+        return $this->afterMaking(function (User $user) {
+            Log::info('making user');
+        })->afterCreating(function (User $user) {
+            Log::info('creating user');
+        });
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Database\Factories\AdminFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(2)->unverified()->admin()->trashed()->create();
+        // AdminFactory::new()->count(10)->create();
+    }
+}
